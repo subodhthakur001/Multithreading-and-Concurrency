2394ee8c12cf8bebf8a8dc29124e074af049a92a
subodh <skthakur.styles619@gmail.com>
2025-06-10 19:57:29 +0530

    basic threads code examples

---
+++ basicthreads/Main.java
@@ -0,0 +1,8 @@
+package basicthreads;
+
+public class Main {
+    public static void main(String[] args){
+        ThreadExample thread1 = new ThreadExample();
+        thread1.run();
+    }
+}
---
+++ basicthreads/ThreadExample.java
@@ -0,0 +1,12 @@
+package basicthreads;
+
+//using runnable
+public class ThreadExample implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Thread 1 is created and running");
+    }
+}
+
+
+
