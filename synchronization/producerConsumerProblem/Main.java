3983525175df08bd23ca531a67bf7d759f029ba7
subodh <skthakur.styles619@gmail.com>
2025-06-10 20:31:38 +0530

    implemented producer consumer problem

--- synchronization/producerConsumerProblem/Main.java
+++ synchronization/producerConsumerProblem/Main.java
@@ -3,12 +3,13 @@ package synchronization.producerConsumerProblem;
 public class Main {
     public static void main(String[] args){
         SharedResource sharedResource = new SharedResource(3);
+        //Producer Thread
         Thread producerThread = new Thread(() -> {
             for(int i = 0; i <= 6; i++){
                 sharedResource.produce(i);
             }
         });
-
+        //Consumer Thread
         Thread consumerThread = new Thread(() -> {
             for(int i = 0; i <= 6; i++){
                 sharedResource.consume();
